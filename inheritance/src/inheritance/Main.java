de83a2e0a0374e2d7e0dec4af16804f0dcaac6c7
Fatma <Fatma@DESKTOP-PRJGK96>
2021-05-09 17:49:17 +0300

    inheritance dosyası

---
+++ inheritance/src/inheritance/Main.java
@@ -0,0 +1,25 @@
+package inheritance;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		IndividualCustomer engin= new IndividualCustomer();
+		engin.customerNumber="12345";
+		
+		CorporateCustomer hepsiBurada= new CorporateCustomer();
+		hepsiBurada.customerNumber="23145";
+		
+		CustomerManager customerManager= new CustomerManager();
+		
+		Customer[] customers= {engin, hepsiBurada};
+		customerManager.addMultiple(customers);
+		
+
+	}
+	
+	
+	
+	
+
+}
